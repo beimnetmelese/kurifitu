a91a5fc2c9d3018273d2679f8c58f2a949f32ea7
Bewnet Addisalem <bewnetaddisalem@gmail.com>
2026-04-04 07:31:19 +0900

    The backend has been deployed

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -50,7 +50,7 @@ export default function AdminLogin({ onLoginSuccess }: LoginProps) {
     setErrorMessage(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/auth/jwt/create/", {
+      const response = await fetch("https://bewnet.pythonanywhere.com/auth/jwt/create/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
